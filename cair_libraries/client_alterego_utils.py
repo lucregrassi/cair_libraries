1cfde66e21449111d48cc4ca17c91089287762ee
lucregrassi <lucre.grassi@gmail.com>
2024-07-09 12:21:20 +0200

    modified alterego client libraries

--- cair_libraries/client_alterego_utils.py
+++ cair_libraries/client_alterego_utils.py
@@ -15,11 +15,13 @@ folder_path = package_path + "/common"
 
 
 class AlteregoClientUtils:
-    def __init__(self, language, port, server_ip, registration_ip):
-        self.language = language
+    def __init__(self, port, server_ip, registration_ip):
         self.server_ip = server_ip
         self.registration_ip = registration_ip
         self.port = port
+        self.dialogue_state_file_path = "session_data/dialogue_state.json"
+        self.speakers_info_file_path = "session_data/speakers_info.json"
+        self.dialogue_statistics_file_path = "session_data/dialogue_statistics.json"
 
     def process_sentence(self, sentence, speakers_info):
         sentence = self.replace_schwa(sentence, speakers_info)
@@ -74,9 +76,11 @@ class AlteregoClientUtils:
 
     # This method performs a GET request to the cloud to get the initial sentence and the dialogue state that will be used
     # for all the speakers. Then, it initializes the speakers stats and speakers info data for the unknown speaker
-    def acquire_initial_state(self):
+    def acquire_initial_state(self, language):
+        json_language = {"language": language}
         # Try to contact the server and retry until the dialogue state is received
-        resp = requests.get("http://" + self.server_ip + ":" + self.port + "/CAIR_hub", verify=False)
+        resp = requests.get("http://" + self.server_ip + ":" + self.port + "/CAIR_hub/start", json=json_language,
+                            verify=False)
         print(resp)
         first_dialogue_sentence = resp.json()["first_sentence"]
         dialogue_state = resp.json()['dialogue_state']
@@ -86,17 +90,18 @@ class AlteregoClientUtils:
             print("S: Waiting for the CAIR server to provide the dialogue state...")
             # Keep on trying to perform requests to the server until it is reachable.
             while not dialogue_state:
-                resp = requests.get("http://" + self.server_ip + ":" + self.port + "/CAIR_hub", verify=False)
+                resp = requests.post("http://" + self.server_ip + ":" + self.port + "/CAIR_hub/start", json=json_language,
+                                     verify=False)
                 dialogue_state = resp.json()['dialogue_state']
                 time.sleep(1)
         # Store the dialogue state in the corresponding file
-        with open(os.path.join(folder_path, "dialogue_state.json"), 'w') as f:
+        with open(self.dialogue_state_file_path, 'w') as f:
             json.dump(dialogue_state, f, ensure_ascii=False, indent=4)
 
         profile_id = "00000000-0000-0000-0000-000000000000"
         # Add the info of the new profile to the file where the key is the profile id and the values are the info (name)
-        with open(os.path.join(folder_path, "speakers_info.json"), 'w') as f:
-            if self.language == "it":
+        with open(self.speakers_info_file_path, 'w') as f:
+            if language == "it-IT":
                 user_name = "Utente"
             else:
                 user_name = "User"
@@ -106,22 +111,22 @@ class AlteregoClientUtils:
         # Initialize dialogue statistics
         dialogue_statistics = DialogueStatistics(profile_id=profile_id)
         # Update the stats in the file
-        with open(os.path.join(folder_path, "dialogue_statistics.json"), 'w') as f:
+        with open(self.dialogue_statistics_file_path, 'w') as f:
             json.dump(dialogue_statistics.to_dict(), f, ensure_ascii=False, indent=4)
         return first_dialogue_sentence
 
     # This method updates the info and the statistics of the users when a new user registers
     def add_speaker_statistics(self, new_speaker_info):
         # Load the information about the already existing users to add the new user
-        with open(os.path.join(folder_path, "speakers_info.json"), 'r') as info:
+        with open(self.speakers_info_file_path, 'r') as info:
             speakers_info = json.load(info)
         # Load the statistics about the interactions with existing users to update them
-        with open(os.path.join(folder_path, "dialogue_statistics.json"), 'r') as stats:
+        with open(self.dialogue_statistics_file_path, 'r') as stats:
             dialogue_statistics = DialogueStatistics(d=json.load(stats))
         # Increase the size of the matrices and of the lists and add the new profile id to the list of mapped speakers
         dialogue_statistics.add_new_speaker_statistics(new_speaker_info.profile_id)
         # Update the stats in the file
-        with open(os.path.join(folder_path, "dialogue_statistics.json"), 'w') as f:
+        with open(self.dialogue_statistics_file_path, 'w') as f:
             json.dump(dialogue_statistics.to_dict(), f, ensure_ascii=False, indent=4)
 
         # Add the info of the new profile to the file where the key is the profile id and the values are the info (name)
@@ -137,13 +142,13 @@ class AlteregoClientUtils:
 
         speakers_info[new_speaker_info.profile_id] = {"name": new_speaker_info.name, "gender": user_gender,
                                                       "age": new_speaker_info.age}
-        with open(script_folder + "/speakers_info.json", 'w') as f:
+        with open(self.speakers_info_file_path, 'w') as f:
             json.dump(speakers_info, f, ensure_ascii=False, indent=4)
 
         return speakers_info, dialogue_statistics
 
     # This function performs the registration of a new speaker on the Microsoft APIs
-    def registration_procedure(self):
+    def registration_procedure(self, language):
         # Establish a socket connection with the registration.py script
         client_registration_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_registration_socket.connect((self.registration_ip, 9091))
@@ -153,7 +158,7 @@ class AlteregoClientUtils:
         new_profile_id = client_registration_socket.recv(256).decode('utf-8')
 
         # ** STEP 2 ** Ask the name to the user
-        if self.language == "it":
+        if language == "it-IT":
             to_say = "S: Per favore, dimmi come ti chiami."
         else:
             to_say = "S: Please, tell me your name."
@@ -162,7 +167,7 @@ class AlteregoClientUtils:
         new_profile_name = client_registration_socket.recv(256).decode('utf-8')
 
         # ** STEP 3 ** Ask the gender to the user
-        if self.language == "it":
+        if language == "it-IT":
             to_say = "Per favore, dimmi quale pronome di genere vuoi che usi quando parlo con te: femminile, maschile o neutro?"
         else:
             to_say = "Please, tell me which gender pronoun should I use when I talk with you: male, female or neutral?"
@@ -171,7 +176,7 @@ class AlteregoClientUtils:
         new_profile_gender = client_registration_socket.recv(256).decode('utf-8')
 
         # ** STEP 4 ** Ask the age to the user
-        if self.language == "it":
+        if language == "it-IT":
             to_say = "Per favore, dimmi quanti anni hai."
         else:
             to_say = "Please, tell your age."
@@ -180,7 +185,7 @@ class AlteregoClientUtils:
         new_profile_age = client_registration_socket.recv(256).decode('utf-8')
 
         # ** STEP 5 ** Ask the user to talk for 20 seconds
-        if self.language == "it":
+        if language == "it-IT":
             to_say = "S: Per favore, parla per 20 secondi in modo che io possa imparare a riconoscere la tua voce."
         else:
             to_say = "S: Please, talk for 20 seconds so that I can learn to recognize your voice."
@@ -188,8 +193,8 @@ class AlteregoClientUtils:
         client_registration_socket.send(b"new_profile_enrollment")
         # Wait for the completion of the enrollment
         print("*** Listening ***")
-        client_registration_socket.recv(256).decode('utf-8')
-        if self.language == "it":
+        client_registration_socket.recv(1024).decode('utf-8')
+        if language == "it-IT":
             to_say = "S: Grazie per aver completato la registrazione " + new_profile_name + \
                      "! D'ora in poi riconoscerò la tua voce!"
         else:
