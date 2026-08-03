57671e21065321857495812fcaffef7647f46b41
Lucrezia Grassi <36965729+lucregrassi@users.noreply.github.com>
2026-08-03 17:17:35 +0200

    changed probability of choosing topics

--- cair_libraries/server_utils.py
+++ cair_libraries/server_utils.py
@@ -308,11 +308,11 @@ class ServerUtils:
                 rand_n = random.uniform(0.0, 1.0)
                 print("Random number to decide what to do:", rand_n)
 
-                if rand_n < 0.7:
+                if rand_n < 0.25:
                     print("** CHOOSING AMONG TOP CONCEPTS with familiarity different from zero")
                     top_concept = True
                     topic_n = self.incremental_familiarity_based_choice(ontology.top_topics, topics_familiarity, False)
-                elif 0.7 <= rand_n < 0.85:
+                elif 0.25 <= rand_n < 0.7:
                     print("** CHOOSING AMONG BROTHERS with familiarity different from zero")
                     brother = True
                     topic_n = self.incremental_familiarity_based_choice(ontology.topics_brothers[prev_topic_number],
